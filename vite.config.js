da93e0eb8ca5daa457d53e2c6b745c15282db734
Aashish Harishchandre <aashish0415@gmail.com>
2026-07-05 16:47:36 -0400

    chore: add empty plugins array to vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 
 // Repo is served from https://<user>.github.io/3DTrainSim/ on GitHub Pages.
 export default defineConfig({
+  plugins: [],
   base: process.env.GITHUB_ACTIONS ? "/3DTrainSim/" : "/",
 });
